b1dd47ce518608dcd5aa782f49a706597802c615
Gurpreet1490 <g.preet08092014@gmail.com>
2022-02-04 11:16:27 +0000

    name changed

---
+++ src/main/java/model/Instructions.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Instructions {
+    private String instructions;
+
+    public Instructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public InstructionType[] getInstruction(){
+        ArrayList<InstructionType> result = new ArrayList<InstructionType>();
+            for (char c: instructions.toCharArray()) {
+                switch (c) {
+                    case 'L':
+                        result.add(InstructionType.LEFT);
+                        break;
+                    case 'R':
+                        result.add(InstructionType.RIGHT);
+                        break;
+                    case 'M':
+                        result.add(InstructionType.MOVE);
+                        break;
+                    default:
+                        throw new unknownInstructionException(c);
+                }
+            }
+        return result.toArray(new InstructionType[result.size()]);
+    }
+}
