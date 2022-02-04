517ab02c90500ff7ce1fa11a11033a55405c4e16
Gurpreet1490 <g.preet08092014@gmail.com>
2022-02-04 11:00:27 +0000

    More Test added

--- src/test/java/RoverTest.java
+++ src/test/java/RoverTest.java
@@ -2,35 +2,32 @@ import model.*;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class RoverTest {
 
-    private  Plateau plateau = new Plateau(5,5);
+    private  Plateau plateau = new Plateau(20,20);
 
-    private static Instruction[] convertToArray(String instruction) {
-        return new RoverInstruction(instruction).getInstruction();
+    @Test
+    public void landingAndMovingRoverShouldSucceedCase1(){
+        Rover rover1 = new Rover("RoverX1");
+
+        rover1.landRoverPlateau(plateau, new Position(10, 10), Direction.NORTH);
+        rover1.command(convertToArray("LMLMLMLMM"));
+        String report = rover1.reportPosition();
+
+        assertEquals("10 11 N", report);
     }
 
     @Test
-    public void letDropRover(){
+    public void landingAndMovingRoverShouldSucceedCase2(){
         Rover rover1 = new Rover("RoverX1");
-        rover1.landRover(plateau, "3 5 E");
+
+        rover1.landRoverPosition(plateau, 3, 3, 'E');
+        rover1.command(convertToArray("MMRMMMRRM"));
+
         String report = rover1.reportPosition();
 
-        assertEquals("3 5 E", report);
-    }
-
-
-    @Test
-    public void moveInstruction(){
-        Rover rover1 = new Rover("RoverX1");
-        rover1.landRover(plateau, 3, 3, 'E');
-        //rover1.command("MMRMMRMRRM");
-        rover1.command(convertToArray("MMRMMRMRRM"));
-        String report = rover1.reportPosition();
-
-        assertEquals("5 1 E", report);
+        assertEquals("5 1 N", report);
     }
 
     @Test
@@ -41,7 +38,6 @@ public class RoverTest {
         assertEquals("Rover not landed!", report);
     }
 
-    //full status with name
     @Test
     public void roverNotDroppedWithName(){
         Rover rover1 = new Rover("RoverX1");
@@ -50,23 +46,51 @@ public class RoverTest {
         assertEquals("RoverX1 Rover not landed!", report);
     }
 
-
-
-    @Test
+    @Test(expected = RuntimeException.class)
     public void movingRoverOverAnotherRoverShouldThrowException(){
         Rover rover1 = new Rover("RoverX1");
         Rover rover2 = new Rover("RoverX2");
 
         rover1.landRover(plateau, "3 5 E");
-
-        try {
-            rover2.landRover(plateau, "3 5 N");
-            assertTrue("Should have thrown before!", false);
-        }
-        catch (RuntimeException ex){
-
-        }
+        rover2.landRover(plateau, "3 5 N");
     }
 
+    @Test(expected = PositionNotFoundException.class)
+    public void movingRoverOutsidePlateauBoundaryShouldThrowException(){
+        Rover rover1 = new Rover("RoverX1");
+
+        rover1.landRover(plateau, "0 0 S");
+        rover1.command(convertToArray("M"));
+    }
+
+    @Test
+    public void Rover2DroppedNextToAnotherShouldSucceed(){
+        Rover rover1 = new Rover("RoverX1");
+        Rover rover2 = new Rover("RoverX2");
+
+        rover1.landRover(plateau, "3 5 E");
+        rover2.landRover(plateau, "4 5 N");
+    }
+
+
+    @Test(expected = PositionNotFoundException.class)
+    public void roverShouldFailToLandOutsidePlateau(){
+        Rover rover1 = new Rover("RoverX1");
+
+        rover1.landRover(plateau, "25 25 E");
+    }
+
+    @Test
+    public void landRoverOnEmptyLocationShouldSucceed(){
+        Rover rover1 = new Rover("RoverX1");
+        rover1.landRover(plateau, "3 5 E");
+        String report = rover1.reportPosition();
+
+        assertEquals("3 5 E", report);
+    }
+
+    private static Instruction[] convertToArray(String instructions) {
+        return new Instructions(instructions).getInstruction();
+    }
 
 }
